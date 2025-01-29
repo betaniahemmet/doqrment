46be85974c6fcd7b668056ae6e81d67aa094a817
henrik björserud <henrikbjorserud@gmail.com>
2025-01-29 21:18:00 +0100

    Tinkering with project structure.

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@
 import os
 
 class Config:
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))     # Define the base directory (project root)
+
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret")
     FLASK_ENV = os.environ.get("FLASK_ENV", "production")
     DATABASE_URL = os.environ.get("DATABASE_URL", "sqlite:///prod_db.sqlite3")
@@ -16,6 +18,7 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     # Development-specific configurations
     LOG_LEVEL = "DEBUG"
+    PROJECT_ROOT = Config.BASE_DIR
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -24,6 +27,7 @@ class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     # Production-specific configurations
     LOG_LEVEL = "WARNING"
+    PROJECT_ROOT = Config.BASE_DIR
 
 class TestingConfig(Config):
     TESTING = True
@@ -32,3 +36,4 @@ class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     # Testing-specific configurations
     LOG_LEVEL = "DEBUG"
+    PROJECT_ROOT = Config.BASE_DIR
