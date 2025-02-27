25a2402f2cd2d0e999ec71a66647ec18acdc2353
henrik björserud <henrikbjorserud@gmail.com>
2025-02-27 20:53:52 +0100

    Added functioning frontend for one view. Implemented react and tailwind. Still have to fix connection do db

---
+++ app/components/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import MoodActivityLog from './MoodActivityLog';
+
+const root = ReactDOM.createRoot(document.getElementById('app'));
+root.render(<MoodActivityLog />);
